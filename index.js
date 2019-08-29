93fa7b71b8b5939704eb57b56c1d03e89b7f5ff8
Jan Bölsche <regular.gonzales@googlemail.com>
2019-08-29 17:09:32 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,31 @@
+const {join, resolve} = require('path')
+const ssbKeys = require('scuttlebot-release/node_modules/ssb-keys')
+const ssbClient = require('scuttlebot-release/node_modules/ssb-client')
+const conf = require('rc')('tre')
+
+module.exports = function(cb) {
+  const configPath = conf.config
+  if (!configPath) {
+    return cb(new Error('.trerc not found, use --config CONFIG'))
+  }
+  const ssbPath = conf.path || join(configPath, '../.tre')
+  ssbKeys.load(join(ssbPath, 'secret'), (err, keys) => {
+    if (err) return cb(err)
+
+    ssbClient(keys, Object.assign({},
+      conf, { manifest: {manifest: 'async'} }
+    ), (err, ssb) => {
+      if (err) return cb(err)
+      ssb.manifest( (err, manifest) => {
+        if (err) return cb(err)
+        ssb.close()
+        ssbClient(keys, Object.assign({},
+          conf, { manifest } 
+        ), (err, ssb) => {
+          if (err) return cb(err)
+          cb(null, ssb, conf, keys)
+        })
+      })
+    })
+  })
+}
